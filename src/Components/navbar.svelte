18d29f48a147f563841b710c4a151ed0b84e7b0d
Azarattum <azarattum@pm.me>
2021-10-04 23:50:59 +0700

    Added navigation bar

---
+++ src/Components/navbar.svelte
@@ -0,0 +1,92 @@
+<script>
+  const tabs = ["Library", "Listen", "Explore"];
+  export let selected = 0;
+</script>
+
+<nav>
+  {#each tabs as tab, i}
+    <button
+      class={tab.toLowerCase()}
+      class:active={selected === i}
+      on:click={() => (selected = i)}
+    >
+      {tab}
+    </button>
+  {/each}
+</nav>
+
+<style lang="postcss">
+  nav {
+    position: fixed;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    bottom: 0;
+    height: 44px;
+    width: 100vw;
+
+    backdrop-filter: blur(16px);
+    background: var(--color-overlay);
+    border-top: solid 1px var(--color-highlight);
+  }
+  :global(.standalone) nav {
+    height: 49px;
+
+    button {
+      padding: 2px 0 2px 0;
+      font-size: var(--font-caption);
+    }
+  }
+  button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: var(--color-text-caption);
+    font-weight: bold;
+    padding: 6px 0 6px 0;
+    width: 128px;
+    font-size: 0;
+
+    border-radius: 8px;
+    transition: color, background-color 0.2s ease;
+
+    &:active {
+      transition: 0s;
+      color: var(--color-text-selected);
+      background-color: var(--color-highlight);
+
+      &::before {
+        background-color: var(--color-text-selected);
+      }
+    }
+
+    &.library::before {
+      mask-image: url("/icons/library.svg");
+    }
+    &.listen::before {
+      mask-image: url("/icons/listen.svg");
+    }
+    &.explore::before {
+      mask-image: url("/icons/explore.svg");
+    }
+  }
+  button::before {
+    display: block;
+    content: "";
+    height: 32px;
+    width: 35px;
+    background-color: var(--color-text-caption);
+
+    mask: no-repeat 50% 50%;
+    mask-size: 26px 26px;
+    pointer-events: none;
+
+    transition: inherit;
+  }
+  button.active {
+    color: var(--color-accent-75);
+    &::before {
+      background-color: var(--color-accent-75);
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
   import Library from "components/library.svelte";
+  import Navbar from "components/navbar.svelte";
+
+  const tabs = [Library];
+  let selected = 0;
 </script>
 
-<Library />
+{#each tabs as tab, i}
+  {#if selected == i}
+    <svelte:component this={tab} />
+  {/if}
+{/each}
+<Navbar bind:selected />
