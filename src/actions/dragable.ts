abafd63ea5b176498cf477a5b8eb99eab55e02ec
Azarattum <azarattum@pm.me>
2021-10-27 00:16:19 +0700

    Implemented draggable scrolling

--- src/actions/dragable.ts
+++ src/actions/dragable.ts
@@ -9,26 +9,17 @@ function childIndex(element: HTMLElement): number {
   );
 }
 
-function kebabize(str: string) {
-  return str
-    .split("")
-    .map((letter, idx) => {
-      return letter.toUpperCase() === letter
-        ? `${idx !== 0 ? "-" : ""}${letter.toLowerCase()}`
-        : letter;
-    })
-    .join("");
-}
-
 export default function draggable(
   node: HTMLElement,
-  { key }: { key: string }
+  { container }: { container: HTMLElement }
 ): { destroy: () => void } {
-  key += "DragIndex";
-  let startPosition = [0, 0];
+  const transformed = new Map<HTMLElement, Animation>();
+  const scrollThreshold = node.offsetHeight * 2;
+
+  let bounds = { top: 0, bottom: innerHeight };
+  let startPosition = [0, 0, 0];
   let target: HTMLElement | null = null;
   let targetIndex: number | null = null;
-  const transformed = new Map<HTMLElement, Animation>();
 
   let index = childIndex(node);
   let holder = node.parentElement;
@@ -36,22 +27,25 @@ export default function draggable(
     index = childIndex(holder);
     holder = holder.parentElement;
   }
-  node.dataset[key] = index.toString();
+  if (node.dataset["dragIndex"] == null) {
+    node.dataset["dragIndex"] = index.toString();
+  }
 
   let lastAnimated: number | null = null;
   let lastDirection: boolean | null = null;
+  let lastEvent: TouchEvent | null = null;
   function applyAnimation(element: HTMLElement | number): number | null {
     if (lastAnimated == null) {
       throw new Error("Trying to apply an animation with no lastAnimated set!");
     }
     if (typeof element === "number") {
-      const id = `[data-${kebabize(key)}="${element}"`;
-      const found = document.querySelector<HTMLElement>(id);
+      const id = `[data-drag-index="${element}"`;
+      const found = container.querySelector<HTMLElement>(id);
       if (!found) return targetIndex;
       element = found;
     }
 
-    const data = element.dataset[key];
+    const data = element.dataset["dragIndex"];
     if (!data) {
       throw new Error(
         "Trying to apply an animation on a non draggable element!"
@@ -98,8 +92,15 @@ export default function draggable(
       rigid();
       node.dispatchEvent(new Event("scrollcancel", { bubbles: true }));
 
+      const rect = container.getBoundingClientRect();
+      bounds = { top: rect.top, bottom: rect.top + rect.height };
       lastAnimated = index;
-      startPosition = [event.touches[0].clientX, event.touches[0].clientY];
+      startPosition = [
+        event.touches[0].clientX,
+        event.touches[0].clientY,
+        container.scrollTop,
+      ];
+
       addEventListener("touchmove", handleMove, { passive: false });
       addEventListener("touchend", handleEnd);
       node.classList.add("dragging");
@@ -129,19 +130,30 @@ export default function draggable(
   }
 
   function handleMove(event: TouchEvent) {
+    lastEvent = event;
     event.preventDefault();
     const { clientX, clientY } = event.touches[0];
+    const normY = Math.min(Math.max(clientY, bounds.top), bounds.bottom - 1);
     const dx = clientX - startPosition[0];
-    const dy = clientY - startPosition[1];
-    node.style.transform = `translate3d(${dx}px, ${dy}px, 0)`;
+    const dy = normY - startPosition[1];
+    const ds = container.scrollTop - startPosition[2];
+    node.style.transform = `translate3d(${dx}px, ${dy + ds}px, 0)`;
 
     const elements = document.elementsFromPoint(
       clientX,
-      clientY
+      normY
     ) as HTMLElement[];
     const element = elements.find(
-      (x) => x != node && Number.isInteger(+(x.dataset[key] || NaN))
+      (x) => x != node && Number.isInteger(+(x.dataset["dragIndex"] || NaN))
     );
+
+    if (clientY - bounds.top < scrollThreshold) {
+      startScroll(false);
+    } else if (clientY > bounds.bottom - scrollThreshold) {
+      startScroll(true);
+    } else if (scrolling) {
+      scrolling = false;
+    }
 
     if (element == target) return;
     target = element as HTMLElement;
@@ -150,6 +162,31 @@ export default function draggable(
       targetIndex = applyAnimation(target);
       select();
     }
+  }
+
+  let scrolling = false;
+  function startScroll(direction: boolean) {
+    if (scrolling) return;
+    scrolling = true;
+    const modifier = direction ? 1 : -1;
+    const barrier = direction ? bounds.bottom : bounds.top;
+
+    let then = Date.now();
+    const scroll = () => {
+      const y = lastEvent?.touches[0].clientY || 0;
+      const rate = Math.pow(
+        1 - (-modifier * (y - barrier)) / scrollThreshold,
+        2
+      );
+
+      const elapsed = Date.now() - then;
+      then = Date.now();
+
+      container.scrollBy(0, (elapsed * modifier * rate) / 2);
+      if (lastEvent) handleMove(lastEvent);
+      if (scrolling) requestAnimationFrame(scroll);
+    };
+    scroll();
   }
 
   function handleEnd() {
@@ -164,6 +201,8 @@ export default function draggable(
     transformed.clear();
     lastDirection = null;
     lastAnimated = null;
+    lastEvent = null;
+    scrolling = false;
 
     if (targetIndex == null || targetIndex == index) return;
     node.dispatchEvent(
