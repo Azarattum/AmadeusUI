b842926bac6f51af3d52cf359ee0ea17164998d9
Azarattum <Azarattum@users.noreply.github.com>
2022-07-28 10:26:34 +0000

    deploy: 604df5cebbcffbb7c903c7a476b0680218705bf9

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 const build = [
-  "/_app/start-571f03a3.js",
-  "/_app/pages/__layout.svelte-f6a6addb.js",
-  "/_app/assets/pages/__layout.svelte-ee4679ba.css",
-  "/_app/error.svelte-c6411e1f.js",
-  "/_app/pages/index.svelte-700a5e39.js",
-  "/_app/assets/pages/index.svelte-79fd83c9.css",
-  "/_app/chunks/index-571d3ec7.js",
-  "/_app/chunks/preload-helper-0e672089.js",
+  "/_app/start-cdb96328.js",
+  "/_app/pages/__layout.svelte-5cd99b7c.js",
+  "/_app/assets/pages/__layout.svelte-c779cd41.css",
+  "/_app/error.svelte-d2599526.js",
+  "/_app/pages/index.svelte-1fac479f.js",
+  "/_app/assets/pages/index.svelte-0aa79675.css",
+  "/_app/chunks/index-2bdb744f.js",
+  "/_app/chunks/preload-helper-dd72e550.js",
   "/_app/chunks/paths-396f020f.js",
-  "/_app/chunks/hls-86082839.js"
+  "/_app/chunks/hls-1049faa6.js"
 ];
 const files$1 = [
   "/fonts/blokk.woff2",
@@ -22,7 +22,7 @@ const files$1 = [
   "/images/splash-plus.jpg",
   "/manifest.json"
 ];
-const version = "1659002862614";
+const version = "1659003979463";
 const files = files$1.filter((x) => x != "/.DS_Store");
 const worker = self;
 const assets = `cache${version}`;
